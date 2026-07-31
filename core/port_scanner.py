85414c6b95a36a23126baf14b230f846b17792ce
Pelihere <pelitalebi.1383@gmail.com>
2026-07-31 11:13:07 +0330

    fix: fix port scanner modlue bugs

--- core/port_scanner.py
+++ core/port_scanner.py
@@ -1,26 +1,29 @@
 import socket
 from concurrent.futures import ThreadPoolExecutor
+import threading
 
 class PortScanner:
     def __init__(self, target):
         self.target = target
         self.open_ports = []
+        self.lock = threading.Lock()
 
-            
     def scan_port(self, port):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(1)
 
-        try:
-            result = sock.connect_ex((self.target, port))
+            try:
+                result = sock.connect_ex((self.target, port))
 
-            if result == 0:
-                with self.lock:
+                if result == 0:
+                    print(f"FOUND {port}")
                     self.open_ports.append(port)
-                    print(f"[+] Port {port} is open")
 
-        finally:
-            sock.close()
+            except Exception as e:
+                print(port, e)
+
+            finally:
+                sock.close()
 
 
     def scan(self, start_port=1, end_port=65535):
@@ -29,6 +32,6 @@ class PortScanner:
         self.open_ports.clear()
 
         with ThreadPoolExecutor(max_workers=100) as executor:
-            executor.map(self.scan_port, range(start_port, end_port + 1))
+            list(executor.map(self.scan_port, range(start_port, end_port + 1)))
 
         return sorted(self.open_ports)
